fd3e18b41359f8887c40379e5018c7fd2844454f
SofieRydmark <you@example.com>
2023-01-22 19:47:32 +0100

    naming updated

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-const body = document.querySelector('body'),
-  modeToggle = document.querySelector('.dark-light-toggle')
+const body = document.querySelector("body"),
+  modeToggle = document.querySelector("dark-light-toggle")
 
-let getMode = localStorage.getItem('mode')
-if (getMode && getMode === 'dark-mode') {
-  body.classList.add('dark')
+let getMode = localStorage.getItem("mode")
+if (getMode && getMode === "dark-mode") {
+  body.classList.add("dark")
 }
 
 // toggle dark and light mode
-modeToggle.addEventListener('click', () => {
-  modeToggle.classList.toggle('active')
-  body.classList.toggle('dark')
+modeToggle.addEventListener("click", () => {
+  modeToggle.classList.toggle("active")
+  body.classList.toggle("dark")
 
   // keep user selected mode even if page refresh
-  if (!body.classList.contains('dark')) {
-    localStorage.setItem('mode', 'light-mode')
+  if (!body.classList.contains("dark")) {
+    localStorage.setItem("mode", "light-mode")
   } else {
-    localStorage.setItem('mode', 'dark-mode')
+    localStorage.setItem("mode", "dark-mode")
   }
 })
+
+const openGithub = () => {
+  window.open("https://github.com/SofieRydmark")
+}
+
+const openLinkedIn = () => {
+  window.open("https://www.linkedin.com/in/sofierydmark/")
+}
