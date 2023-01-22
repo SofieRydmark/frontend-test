db5911dddbec48da7347e1ad12b2d48e656623ec
SofieRydmark <you@example.com>
2023-01-22 13:24:41 +0100

    init git repo

---
+++ script.js
@@ -0,0 +1,20 @@
+const body = document.querySelector('body'),
+  modeToggle = document.querySelector('.dark-light-toggle')
+
+let getMode = localStorage.getItem('mode')
+if (getMode && getMode === 'dark-mode') {
+  body.classList.add('dark')
+}
+
+// toggle dark and light mode
+modeToggle.addEventListener('click', () => {
+  modeToggle.classList.toggle('active')
+  body.classList.toggle('dark')
+
+  // keep user selected mode even if page refresh
+  if (!body.classList.contains('dark')) {
+    localStorage.setItem('mode', 'light-mode')
+  } else {
+    localStorage.setItem('mode', 'dark-mode')
+  }
+})
